7c06af2ecf7632251271075ac721be921808a262
Sergio Arcos <sergio.arcos@gmail.com>
2020-01-26 11:04:13 +0900

    chore: extract index.js

--- index.js
+++ index.js
@@ -2,29 +2,26 @@ require('dotenv').config();
 
 const moment = require('moment-timezone');
 const readline = require('readline');
-const {getAuth} = require('./lib/google-oauth2.js');
-const {listEvents} = require('./lib/google-calendar.js');
-const {show, persist} = require('./lib/jobcan.js');
+const {authenticate} = require('./lib/google-oauth2.js');
+const {getEventList} = require('./lib/google-calendar.js');
+const {display, persist} = require('./lib/jobcan.js');
 
 
-function shouldPersist(data) {
-  show(data);
-
-  return new Promise((resolve, reject) => {
+async function askIfContinue(question, accepted) {
+  return new Promise((resolve) => {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     });
-    rl.question('Do you want to persist the information into JobCan? (y/N) ', (answer) => {
+    rl.question(question, (answer) => {
       rl.close();
-      if (answer === 'y' || answer === 'Y') return resolve(data);
-      reject('Done!');
+      resolve(accepted.find(e => e === answer));
     });
   });
 }
 
 
-async function main() {
+function input() {
   let unit = 'week';
   let timeMin = moment().subtract(1, unit).startOf(unit).toISOString();
   let timeMax = moment().subtract(1, unit).endOf(unit).toISOString();
@@ -33,13 +30,29 @@ async function main() {
   if (myArgs[0]) timeMin = moment(myArgs[0]).toISOString();
   if (myArgs[1]) timeMax = moment(myArgs[1]).toISOString();
 
+  return { timeMin, timeMax };
+}
+
+
+async function main() {
+  const { timeMin, timeMax } = input();
+
   console.log(`With locale ${moment.locale()}, timezone ${moment().format('Z')}`);
   console.log(`Search between ${timeMin} and ${timeMax}`);
 
-  await getAuth()
-    .then(auth => listEvents(auth, timeMin, timeMax))
-    .then(data => shouldPersist(data))
-    .then(data => persist(data));
+  const auth = await authenticate();
+  const events = await getEventList(auth, timeMin, timeMax);
+
+  display(events);
+
+  const question = 'Do you want to persist the information into JobCan? (y/N) ';
+  const accepted = ['y', 'Y', 'yes'];
+  const shouldPersist = await askIfContinue(question, accepted);
+
+  if (!shouldPersist) return;
+
+  await persist(events);
 }
+
 
 main().catch(console.error);
